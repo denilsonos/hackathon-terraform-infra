32c275053f2a82caf656c76d06eaec8adc060d1e
Denilson Oliveira <denilson@MACECOC02FR03UM.local>
2024-07-21 13:39:19 -0300

    update s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-4soat-s3"
+    bucket  = "terraform-4soat-hackathon"
     key     = "terraform/state/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
